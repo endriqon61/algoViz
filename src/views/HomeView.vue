8d1405933857b7d6d53ff297866b7b0b5ccfffaa
endriqon61 <endritshaqiri61@gmail.com>
2023-03-16 09:32:22 +0100

    done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,12 @@
   <main class="bg-indigo-50 h-full overflow-x-hidden overflow-y-scroll">
 
   <header class="flex justify-start items-end">
-    <div class="absolute inset-0 bg-blue-900 z-10 mix-blend-multiply"></div>
+    <div class="absolute inset-0 bg-black z-10 opacity-75"></div>
     <img src="/background.jpg" class="absolute h-full w-full object-cover object-center"/>
     <div class="p-4 m-8 z-30">
-      <h1 class="text-8xl text-blue-50 mb-8">VisuAL</h1>
-      <p class="text-3xl mb-12 text-blue-50">The best algorithm visualizing tool on the web</p>
-      <p class="text-md text-blue-100 max-w-3xl">Experience the power of algorithms like never before with our interactive visualization website. Discover complex concepts in a user-friendly and engaging way. Try it now and take your coding skills to the next level!</p>
+      <h1 class="text-8xl text-indigo-50 mb-12 font-bold">VisuAL</h1>
+      <p class="text-3xl mb-6 text-indigo-50">The best algorithm visualizing tool on the web</p>
+      <p class="text-md text-blue-200 max-w-3xl">Experience the power of algorithms like never before with our interactive visualization website. Discover complex concepts in a user-friendly and engaging way. Try it now and take your coding skills to the next level!</p>
     </div>
   </header>
   <div class="flex justify-center items-center h-full">
